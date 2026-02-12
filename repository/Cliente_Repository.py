5fdbe62a5cdfba85a1e09c5d9bf594c923c8da18
Juan Miguel <oliveirajuan243@gmail.com>
2026-02-12 15:25:00 -0300

    save de cliente

--- repository/Cliente_Repository.py
+++ repository/Cliente_Repository.py
@@ -1,4 +1,29 @@
-
+from neo4j import RoutingControl
+from datetime import date
 class Cliente_Repository:   
-    def save():
-        return "<h1>Hello, World! em cliente repository</h1>"
+    def save(driver, cliente, costureira, medidas, roupa, valor):
+        dia= date.today()
+        if cliente:
+            driver.execute_query(
+                """ MERGE(d:date {name:$dia})
+                    MERGE (c:cliente {name: $cliente})
+                    MERGE (cs:costureira {name: $costureira})
+                    MERGE (m:medidas {name:Medida, medida:$medidas})
+                    MERGE (r:roupa {name:Roupa, descricao:$roupa})
+                    MERGE (v:valor {name:$valor})
+                    MERGE (p:pagamento {name:$pagamemento})
+                    MERGE (d)->[:DIA_DA_VENDA]->(c)
+                    MERGE (c)-[:COSTURAR_COM]->(cs)
+                    MERGE (cs)-[:MEDIU]->(m)
+                    MERGE (m)-[:Para_fazer]->(r)
+                    MERGE (v)-[:VALOR]->(p)
+                """,
+                dia=dia,
+                cliente=cliente,
+                costureira=costureira,
+                medida = medidas,
+                roupa = roupa,
+                valor = valor,
+                database_="neo4j"
+            )
+        
