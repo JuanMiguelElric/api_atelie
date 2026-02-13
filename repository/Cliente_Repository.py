a535f53e7c95ad6e84e79ad1634eea3d81c9ddd3
Juan Miguel <oliveirajuan243@gmail.com>
2026-02-13 13:49:35 -0300

    aaaa

--- repository/Cliente_Repository.py
+++ repository/Cliente_Repository.py
@@ -1,29 +1,33 @@
-from neo4j import RoutingControl
 from datetime import date
-class Cliente_Repository:   
+
+class Cliente_Repository:
+
+    @staticmethod
     def save(driver, cliente, costureira, medidas, roupa, valor):
-        dia= date.today()
+        dia = date.today()
+
         if cliente:
             driver.execute_query(
-                """ MERGE(d:date {name:$dia})
-                    MERGE (c:cliente {name: $cliente})
-                    MERGE (cs:costureira {name: $costureira})
-                    MERGE (m:medidas {name:Medida, medida:$medidas})
-                    MERGE (r:roupa {name:Roupa, descricao:$roupa})
-                    MERGE (v:valor {name:$valor})
-                    MERGE (p:pagamento {name:$pagamemento})
-                    MERGE (d)->[:DIA_DA_VENDA]->(c)
-                    MERGE (c)-[:COSTURAR_COM]->(cs)
-                    MERGE (cs)-[:MEDIU]->(m)
-                    MERGE (m)-[:Para_fazer]->(r)
-                    MERGE (v)-[:VALOR]->(p)
+                """
+                MERGE (d:dia {data:$dia})
+                MERGE (c:cliente {name:$cliente})
+                MERGE (cs:costureira {name:$costureira})
+                MERGE (m:medidas {name:$medida, medida:$medida})
+                MERGE (r:roupa {name:$roupa, descricao:$roupa})
+                MERGE (v:valor {name:$valor})
+                MERGE (p:pagamento {name:'PIX'})
+                
+                MERGE (d)-[:DIA_DA_VENDA]->(c)
+                MERGE (c)-[:COSTURAR_COM]->(cs)
+                MERGE (cs)-[:MEDIU]->(m)
+                MERGE (m)-[:PARA_FAZER]->(r)
+                MERGE (v)-[:VALOR_DE]->(p)
                 """,
                 dia=dia,
                 cliente=cliente,
                 costureira=costureira,
-                medida = medidas,
-                roupa = roupa,
-                valor = valor,
+                medida=medidas,
+                roupa=roupa,
+                valor=valor,
                 database_="neo4j"
             )
-        
