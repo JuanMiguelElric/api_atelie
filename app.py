68b2b539c612fd6f493f51a373109602882d7f77
Juan Miguel <oliveirajuan243@gmail.com>
2026-02-13 10:12:02 -0300

    api quase finalizada

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from repository.Cliente_Repository import Cliente_Repository
+from controller.cliente.ClienteController import ClienteController
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return Cliente_Repository.save()
+
+@app.route("/submit_form", methods=["POST"])
+def submit_form():
+    return ClienteController.create_post()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
---
+++ controller/cliente/ClienteController.py
@@ -0,0 +1,23 @@
+from database.database import get_driver
+from repository.Cliente_Repository import Cliente_Repository
+from flask import request, jsonify
+
+class ClienteController:
+
+    @staticmethod
+    def create_post():
+        with get_driver() as driver:
+            cliente = request.form.get('cliente')
+            costureira = request.form.get('costureira')
+            medida = request.form.get('medida')
+            roupa = request.form.get('roupa')
+            valor = request.form.get('valor')
+
+            if not cliente or not costureira or not medida or not roupa or not valor:
+                return jsonify({"error": "Campos vazios"}), 400
+
+            Cliente_Repository.save(
+                driver, cliente, costureira, medida, roupa, valor
+            )
+
+            return jsonify({"message": "Cliente salvo com sucesso"}), 201
